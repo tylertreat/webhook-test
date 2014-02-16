b35dcb5d79542b0a637f40b0eee91c7da6ef4e0d
Tyler Treat <ttreat31@gmail.com>
2014-02-16 17:45:53 -0600

    Setup simple flask handler

---
+++ main.py
@@ -0,0 +1,19 @@
+import logging
+
+from flask import Flask
+from flask import request
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+@app.route('/push', methods=['POST'])
+def push():
+    logging.error('request: %s' % request.data)
+    return request.data, 200
+
+if __name__ == '__main__':
+    app.run()
