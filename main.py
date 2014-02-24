5575528bd903f296f835b9bd85be34b19ea8d6e1
Tyler Treat <ttreat31@gmail.com>
2014-02-23 21:12:47 -0600

    Testing webhook

--- main.py
+++ main.py
@@ -2,22 +2,20 @@ import logging
 
 from flask import Flask
 from flask import request
+
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return 'Hello World'
 
 
 @app.route('/push', methods=['POST'])
 def push():
     logging.error('request: %s' % request.data)
     return request.data, 200
-
-
-def foo():
-    return 1 + 1
 
 
 if __name__ == '__main__':
