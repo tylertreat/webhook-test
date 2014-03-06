ad5b19221701bfc1f321b99616bf46ac9b15c75a
Tyler Treat <ttreat31@gmail.com>
2014-03-05 23:24:17 -0600

    Add settings.py

---
+++ settings.py
@@ -0,0 +1,10 @@
+
+try:
+    import settingslocal
+except ImportError:
+    settingslocal = None
+
+if settingslocal:
+    for setting in dir(settingslocal):
+        globals()[setting.upper()] = getattr(settingslocal, setting)
+
